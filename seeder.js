006f5ae220472bbbe5b0d71af3588d4b3b25144a
badgersmans <shawnlaw6669@gmail.com>
2020-10-10 09:12:16 +0800

    initial commit

---
+++ seeder.js
@@ -0,0 +1,28 @@
+const mocker = require('mocker-data-generator').default;
+const fs = require('fs');
+
+
+const person = {
+    name: {
+        faker: 'name.findName()'
+    },
+    email: {
+        faker: 'internet.email()'
+    }
+};
+
+mocker()
+    .schema('person', person, 5000000)
+    .build((err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            fs.writeFile('db.json', JSON.stringify(data), 'utf8', (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Done');
+                }
+            });
+        }
+    })
